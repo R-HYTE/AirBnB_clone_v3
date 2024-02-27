4b6f157b1c5b81da898e040fcee321bba916b5bf
R_HYTE <polycarpwathuta39@gmail.com>
2024-02-27 13:05:53 +0300

    fix bug

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,6 @@
 """Defines the app_views Blueprint for API v1."""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
